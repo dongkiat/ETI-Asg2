5dcd51425e418638a75b72a02b5d31243dc01576
dongkiat <dongkiatlim@gmail.com>
2022-01-24 05:06:26 +0800

    test: initiate & add test for get index page

---
+++ main-interface/app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("./backend/app");
+
+describe("Get index page (/)", () => {
+  test("responds with a 200 status code", (done) => {
+    request(app.app).get("/").expect(200, done);
+  });
+
+  afterAll((done) => {
+    app.mysqlTutorConnection.end(done);
+    app.redisClient.disconnect(done);
+  });
+});
