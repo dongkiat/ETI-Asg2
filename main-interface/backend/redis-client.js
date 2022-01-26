8d54e433e9551c1871bbaa55bbe2ef38d29153b1
dongkiat <dongkiatlim@gmail.com>
2022-01-26 16:06:33 +0800

    feat: add redis admin client

--- main-interface/backend/redis-client.js
+++ main-interface/backend/redis-client.js
@@ -5,7 +5,9 @@ if (process.env.NODE_ENV !== "production") {
 const util = require("./util");
 
 const redis = require("redis");
-var redisClient = redis.createClient({
+
+// Session Client -----------------------------------------------------------
+const redisSessionClient = redis.createClient({
   url:
     "redis://" +
     process.env.REDIS_SESSION_HOST +
@@ -17,9 +19,9 @@ var redisClient = redis.createClient({
   },
 });
 
-redisClient.connect();
+redisSessionClient.connect();
 
-redisClient.on("connect", function (err) {
+redisSessionClient.on("connect", function (err) {
   console.log(
     "- Connected to redis session server successfully on port " +
       process.env.REDIS_SESSION_PORT +
@@ -29,7 +31,7 @@ redisClient.on("connect", function (err) {
   );
 });
 
-redisClient.on("error", function (err) {
+redisSessionClient.on("error", function (err) {
   console.error(
     "- Could not establish a connection with redis session server. " +
       err +
@@ -37,7 +39,47 @@ redisClient.on("error", function (err) {
       util.getCurrentDateTime() +
       ") -"
   );
-  // process.exit(1);
 });
 
-module.exports = redisClient;
+// Admin Client -------------------------------------------------------------
+const redisAdminClient = redis.createClient({
+  url:
+    "redis://" +
+    process.env.REDIS_ADMIN_HOST +
+    ":" +
+    process.env.REDIS_ADMIN_PORT,
+  legacyMode: true,
+  socket: {
+    reconnectStrategy: (retries) => Math.min(retries * 2000, 2000),
+  },
+});
+
+redisAdminClient.connect();
+
+redisAdminClient.on("connect", function (err) {
+  console.log(
+    "- Connected to redis admin server successfully on port " +
+      process.env.REDIS_ADMIN_PORT +
+      " . (" +
+      util.getCurrentDateTime() +
+      ") -"
+  );
+});
+
+redisAdminClient.on("error", function (err) {
+  console.error(
+    "- Could not establish a connection with redis admin server. " +
+      err +
+      " . (" +
+      util.getCurrentDateTime() +
+      ") -"
+  );
+});
+
+function authenticateAdmin(userID, password) {
+  var authenticated = true;
+
+  return authenticated;
+}
+
+module.exports = { redisSessionClient, authenticateAdmin };
