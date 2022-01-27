60b06e4df1c6847bf92f89bdad4305b8e386e188
dongkiat <dongkiatlim@gmail.com>
2022-01-28 02:05:33 +0800

    chore: update dependency code

--- main-interface/backend/server.js
+++ main-interface/backend/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const mysqlHandler = require("./mysql-connection");
 const redisHandler = require("./redis-client");
 
